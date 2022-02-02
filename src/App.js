4f28c508b18129b458407f65d364bb933dcdc855
Olga Seledets <olga.seledets@outlook.com>
2022-02-02 19:06:16 +0300

    Get data from managed input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, {useState} from "react";
 import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/button/input/MyInput";
+import "./styles/App.css";
 
 function App() {
-  const [value, setValue] = useState('inputs text')
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript', body: "Description"},
+    {id: 2, title: 'JavaScript-2', body: "Description"},
+    {id: 3, title: 'JavaScript-3', body: "Description"},
+  ])
+
+  const [title, setTitle] = useState('');
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+    console.log(title)
+  }
 
   return (
     <div className="App">
-      <h1>{value}</h1>
-      <input type="text" value={value} onChange={event => setValue(event.target.value)}></input>
-      <Counter/>
+      <form>
+        <MyInput value={title} onChange={e => setTitle(e.target.value)} type="text" placeholder="Post name"/>
+        <MyInput type="text" placeholder="Post description"/>
+        <MyButton onClick={addNewPost}>Create post</MyButton>
+      </form>
+      <PostList posts={posts} title='List posts 1'/>
     </div>
   );
 }
