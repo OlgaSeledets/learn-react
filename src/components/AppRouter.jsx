89f5389e7745c6cfa7d7bffc050cab24ba8b1be6
Olga Seledets <olga.seledets@outlook.com>
2022-06-08 17:12:14 +0300

    Add login page

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,13 +3,28 @@ import {BrowserRouter, Routes, Route, Link, Navigate} from "react-router-dom";
 import About from "../pages/About";
 import PostIdPage from "../pages/PostIdPage";
 import Posts from "../pages/Posts";
+import { routes } from "../router/routes";
+import { publicRoutes, privateRoutes } from "../router/routes";
 
 const AppRouter = () => {
     return (
         <Routes>
-            <Route path="/about" element={<About />} />
-            <Route exact path="/posts" element={<Posts />} />
-            <Route exact path="/posts/:id" element={<PostIdPage />} />
+            {privateRoutes.map((route, pos) =>
+                <Route
+                    element={route.element}
+                    path={route.path}
+                    exact={route.exact}
+                    key={pos}
+                />
+            )}
+            {publicRoutes.map((route, pos) =>
+                <Route
+                    element={route.element}
+                    path={route.path}
+                    exact={route.exact}
+                    key={pos}
+                />
+            )}
             <Route path="*" element={<Posts />} />
         </Routes>
     );
