a3637a479d925b99140c5edbe4f8daa81942471c
Olga Seledets <olga.seledets@outlook.com>
2022-06-08 14:10:02 +0300

    Add dynamic navigation and load comments to a post

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import {BrowserRouter, Routes, Route, Link, Navigate} from "react-router-dom";
 import About from "../pages/About";
+import PostIdPage from "../pages/PostIdPage";
 import Posts from "../pages/Posts";
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/about" element={<About />} />
-            <Route path="/posts" element={<Posts />} />
+            <Route exact path="/posts" element={<Posts />} />
+            <Route exact path="/posts/:id" element={<PostIdPage />} />
             <Route path="*" element={<Posts />} />
         </Routes>
     );
